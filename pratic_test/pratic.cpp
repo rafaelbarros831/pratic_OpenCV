96390efcfb22ecefb4f1132e2791414d86818c7b
rafaelbarros831 <rafasigma80@gmail.com>
2026-05-27 20:56:32 -0300

    Criando círculo.

--- pratic_test/pratic.cpp
+++ pratic_test/pratic.cpp
@@ -8,21 +8,38 @@ int main() {
 	VideoCapture camera(0, CAP_DSHOW);
 
 	Mat imagem_webcam;
-
-	while(true){
-
-
-		camera >> imagem_webcam;
-
-		imshow("Webcam", imagem_webcam);
+	Mat circulo = Mat::zeros(500, 500, CV_8UC3);
 
 
-		if (waitKey(30) == 114 || waitKey(30) == 82) {
-			std::cout << "A tecla r foi pressionada.";
-			break;
+		while (true) {
+
+
+			camera >> imagem_webcam;
+
+			imshow("Webcam", imagem_webcam);
+
+			
+			if (waitKey(30) == 114 || waitKey(30) == 82) {
+				std::cout << "A tecla r foi pressionada.";
+				break;
+			}
+
+			else if(waitKey(30) == 84){
+
+				int raio_circulo = 100;
+				int espessura_circulo = 80;
+
+				Point centro_circulo(250, 250);
+				Scalar cor_circulo(0, 255, 0);
+
+				circle(circulo, centro_circulo, raio_circulo, cor_circulo, espessura_circulo);
+
+				imshow("MyCircle", circulo);
+				waitKey(0);
+
+			}
+
 		}
-
-	 }
 
 	camera.release();
 	destroyAllWindows();
