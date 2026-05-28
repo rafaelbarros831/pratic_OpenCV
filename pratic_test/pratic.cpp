bc88d11a76a937619975c0f10f5bb7070eb09c5b
rafaelbarros831 <rafasigma80@gmail.com>
2026-05-27 21:29:50 -0300

    Pequena alteração.

--- pratic_test/pratic.cpp
+++ pratic_test/pratic.cpp
@@ -3,13 +3,29 @@
 
 using namespace cv;
 
+void circulo() {
+
+	Mat circulo = Mat::zeros(500, 500, CV_8UC3);
+
+	int raio_circulo = 100;
+	int espessura_circulo = 70;
+
+	Point centro_circulo(250, 250);
+	Scalar cor_circulo(0, 255, 0);
+
+	circle(circulo, centro_circulo, raio_circulo, cor_circulo, espessura_circulo);
+
+	imshow("MyCircle", circulo);
+	waitKey(0);
+
+}
+
 int main() {
 
 	VideoCapture camera(0, CAP_DSHOW);
 
 	Mat imagem_webcam;
-	Mat circulo = Mat::zeros(500, 500, CV_8UC3);
-
+	
 
 		while (true) {
 
@@ -26,16 +42,7 @@ int main() {
 
 			else if(waitKey(30) == 84){
 
-				int raio_circulo = 100;
-				int espessura_circulo = 80;
-
-				Point centro_circulo(250, 250);
-				Scalar cor_circulo(0, 255, 0);
-
-				circle(circulo, centro_circulo, raio_circulo, cor_circulo, espessura_circulo);
-
-				imshow("MyCircle", circulo);
-				waitKey(0);
+				circulo();
 
 			}
 
